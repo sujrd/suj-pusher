03e1661f30d649babf3582e7e1bcb5cfaeb3b33e
Horacio Sanson <horacio@skillupjapan.co.jp>
2014-01-08 12:45:00 +0900

    Use fibers to untangle the callback spagetti a little.

--- lib/suj/pusher/daemon.rb
+++ lib/suj/pusher/daemon.rb
@@ -5,6 +5,7 @@ require 'openssl'
 require 'em-hiredis'
 require "multi_json"
 require 'fileutils'
+require 'fiber'
 
 module Suj
   module Pusher
@@ -19,7 +20,9 @@ module Suj
         @last_feedback = Time.now
         @last_sandbox_feedback = Time.now
         EM.run do
-          wait_msg
+          Fiber.new {
+            loop
+          }.resume
         end
       end
 
@@ -34,32 +37,82 @@ module Suj
 
       private
 
-      def wait_msg
-        defer = redis.brpop "#{redis_namespace}:#{MSG_QUEUE}", 0
+      def loop
+        msg = get_msg
+        return next_tick { loop } if msg.nil?
+        conn = get_connection(msg)
+        return next_tick { loop } if conn.nil?
+        conn.deliver(msg)
+        next_tick { loop }
+      end
+
+      def next_tick(&blk)
+        EM.next_tick { Fiber.new { blk.call }.resume }
+      end
+
+      def msg_queue
+        @msg_queue ||= "#{redis_namespace}:#{MSG_QUEUE}"
+      end
+
+      def get_msg
+        f = Fiber.current
+        defer = redis.brpop msg_queue, 0
         defer.callback do |_, msg|
+          info "Received message"
           begin
-            info "RECEIVED MESSAGE"
             data = Hash.symbolize_keys(MultiJson.load(msg))
-            info "GET CONNECTION"
-            conn = pool.get_connection(data)
-            conn.deliver(data)
-            info "SENT MESSAGE"
-            retrieve_feedback(data)
+            f.resume(data)
           rescue MultiJson::LoadError
-            warn("Received invalid json data, discarding msg")
-          rescue ConnectionPool::UnknownConnection
-            warn("Could not determine connetion type for message")
+            warn("Message has bad json format, discarding")
+            f.resume(nil)
           rescue => e
-            error("Error sending notification : #{e}")
-            error e.backtrace
+            error(e)
+            f.resume(nil)
           end
-          EM.next_tick { wait_msg }
         end
         defer.errback do |e|
           error e
-          EM.next_tick { wait_msg }
+          f.resume(nil)
+        end
+        return Fiber.yield
+      end
+
+      def get_connection(options)
+        info "Get connection"
+        begin
+          return pool.get_connection(options)
+        rescue ConnectionPool::UnknownConnection
+          error "Could not find connection"
+          return nil
         end
       end
+
+      # def wait_msg
+      #   defer = redis.brpop "#{redis_namespace}:#{MSG_QUEUE}", 0
+      #   defer.callback do |_, msg|
+      #     begin
+      #       info "RECEIVED MESSAGE"
+      #       data = Hash.symbolize_keys(MultiJson.load(msg))
+      #       info "GET CONNECTION"
+      #       conn = pool.get_connection(data)
+      #       conn.deliver(data)
+      #       info "SENT MESSAGE"
+      #       retrieve_feedback(data)
+      #     rescue MultiJson::LoadError
+      #       warn("Received invalid json data, discarding msg")
+      #     rescue ConnectionPool::UnknownConnection
+      #       warn("Could not determine connetion type for message")
+      #     rescue => e
+      #       error("Error sending notification : #{e}")
+      #       error e.backtrace
+      #     end
+      #     EM.next_tick { wait_msg }
+      #   end
+      #   defer.errback do |e|
+      #     error e
+      #     EM.next_tick { wait_msg }
+      #   end
+      # end
 
       def retrieve_feedback(msg)
         if msg.has_key?(:cert)
